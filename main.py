327f84058f1b934d2473a36bfd957750e85229b1
peterdougstuart <peter.stuart@gmail.com>
2025-04-08 13:22:53 +0100

    corrected typo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from file_io.analyser import BaseAnalyserForm
 
 
 class AnalyserForm(BaseAnalyserForm):
-    def analyze(self, path):
+    def analyse(self, path):
 
         self.log_text(f"Starting analysis of: {path}")
         # ... do something ...
